2f53fc6d003adf27dc64e7d99b202f2f9153e969
iamkiran2210 <kiranbabuchalla21@gmail.com>
2024-02-07 14:30:20 +0530

    feat: Added Functions

--- new.cpp
+++ new.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+void hello_world();
+
 int main()
 {
-  cout<<"Hello World!"<<endl;
+  hello_world();
+  hello_world();
+  hello_world();
+  hello_world();
+  
+  return 0;
+}
+
+void hello_world()
+{
+   cout<<"Hello World!"<<endl;
 }
