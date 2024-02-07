3a828fe03d28c30dbb5cb988276e652134b5f059
iamkiran2210 <159239672+iamkiran2210@users.noreply.github.com>
2024-02-07 14:41:22 +0530

    Update new.js

---
+++ new.js
@@ -0,0 +1,5 @@
+// Generate a random number between 1 and 100
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// Display the random number in the console
+console.log("Random number:", randomNumber);
